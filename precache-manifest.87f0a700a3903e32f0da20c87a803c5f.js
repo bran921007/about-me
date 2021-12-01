a699edb3bc7241ff8fc2e56026bfb748ecfb35d4
Francisco Perez <francisco@square1.io>
2021-12-01 01:29:49 -0400

    Updates

---
+++ precache-manifest.87f0a700a3903e32f0da20c87a803c5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf34a19518d99ad303d000b5761f6971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "246ad8a901691020cb9d",
+    "url": "/static/css/main.a04bce8c.chunk.css"
+  },
+  {
+    "revision": "871d376419b8c2c73c48",
+    "url": "/static/js/2.10558a29.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.10558a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "246ad8a901691020cb9d",
+    "url": "/static/js/main.5b289545.chunk.js"
+  },
+  {
+    "revision": "cb3ccd3eb684c83ffcee",
+    "url": "/static/js/runtime-main.823c8cbe.js"
+  }
+]);
