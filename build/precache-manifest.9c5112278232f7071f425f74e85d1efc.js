5ad621e704b37f77249d5e17cc1f0d3596fda647
Francisco Perez <francisco@square1.io>
2021-12-12 03:10:33 -0400

    update content

---
+++ build/precache-manifest.9c5112278232f7071f425f74e85d1efc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccbd80af8651d1dcad580c74a5a754d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ce15d3f6ae91b4ff98a",
+    "url": "/static/css/main.a04bce8c.chunk.css"
+  },
+  {
+    "revision": "871d376419b8c2c73c48",
+    "url": "/static/js/2.10558a29.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.10558a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce15d3f6ae91b4ff98a",
+    "url": "/static/js/main.3a5637d6.chunk.js"
+  },
+  {
+    "revision": "cb3ccd3eb684c83ffcee",
+    "url": "/static/js/runtime-main.823c8cbe.js"
+  }
+]);
---
+++ src/App/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { ThemeProvider } from './AppContext';
+import { Toggle, Content, Buttons, Footer, Particles } from './../components';
+import './app.scss';
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <div className='app'>
+        <Toggle />
+        <Content />
+        <Buttons />
+        <Particles />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default App;
